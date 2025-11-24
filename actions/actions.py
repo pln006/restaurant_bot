d0899e59da23112c31760820f29288b3bb754162
pln006 <2025pln006@gmail.com>
2025-11-24 12:13:20 -0400

    Update actions.py

--- actions/actions.py
+++ actions/actions.py
@@ -1,4 +1,6 @@
 from rasa_sdk import Action
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
 import mysql.connector
 import os
 
@@ -7,17 +9,20 @@ class ActionGuardarReserva(Action):
     def name(self):
         return "action_guardar_reserva"
 
-    def run(self, dispatcher, tracker, domain):
+    def run(self, dispatcher: CollectingDispatcher, tracker, domain):
+
         personas = tracker.get_slot("personas")
         fecha = tracker.get_slot("fecha")
         hora = tracker.get_slot("hora")
 
+        # Conexión a MySQL
         db = mysql.connector.connect(
             host=os.getenv("DB_HOST"),
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASSWORD"),
             database=os.getenv("DB_NAME")
         )
+
         cursor = db.cursor()
         cursor.execute(
             "INSERT INTO reservas (personas, fecha, hora) VALUES (%s, %s, %s)",
@@ -27,6 +32,6 @@ class ActionGuardarReserva(Action):
         cursor.close()
         db.close()
 
-        dispatcher.utter_message("Tu reserva fue guardada correctamente.")
+        dispatcher.utter_message("Tu reserva ha sido registrada correctamente.")
 
         return []
